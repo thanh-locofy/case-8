0d41964b815d26d2db585b64147e4e4f9f5a250f
thanh-locofy <thanh-locofy>
2024-04-02 14:55:11 +0000

    Pushed from Locofy Builder

--- src/components/DashboardNavigation.tsx
+++ src/components/DashboardNavigation.tsx
@@ -8,53 +8,53 @@ const DashboardNavigation: FunctionComponent = () => {
         <div className={styles.navigation}>Navigation</div>
       </div>
       <div className={styles.accountLinks}>
-        <img className={styles.dashboard2Icon} alt="" src="/dashboard-2.svg" />
-        <div className={styles.dashboard}>Dashboard</div>
+        <img
+          className={styles.dashboard2Icon}
+          loading="lazy"
+          alt=""
+          src="/dashboard-2-5.svg"
+        />
+        <div className={styles.dashboard}>Log-out</div>
       </div>
       <div className={styles.accountLinks1}>
         <img
           className={styles.dashboard2Icon1}
           loading="lazy"
           alt=""
-          src="/dashboard-2-1.svg"
+          src="/dashboard-2-4.svg"
         />
-        <div className={styles.dashboard1}>Order History</div>
+        <div className={styles.dashboard1}>Settings</div>
       </div>
       <div className={styles.accountLinks2}>
         <img
           className={styles.dashboard2Icon2}
           loading="lazy"
           alt=""
-          src="/dashboard-2-2.svg"
+          src="/dashboard-2-3.svg"
         />
-        <div className={styles.dashboard2}>Wishlist</div>
+        <div className={styles.dashboard2}>Shopping Cart</div>
       </div>
       <div className={styles.accountLinks3}>
         <img
           className={styles.dashboard2Icon3}
           loading="lazy"
           alt=""
-          src="/dashboard-2-3.svg"
+          src="/dashboard-2-2.svg"
         />
-        <div className={styles.dashboard3}>Shopping Cart</div>
+        <div className={styles.dashboard3}>Wishlist</div>
       </div>
       <div className={styles.accountLinks4}>
         <img
           className={styles.dashboard2Icon4}
           loading="lazy"
           alt=""
-          src="/dashboard-2-4.svg"
+          src="/dashboard-2-1.svg"
         />
-        <div className={styles.dashboard4}>Settings</div>
+        <div className={styles.dashboard4}>Order History</div>
       </div>
       <div className={styles.accountLinks5}>
-        <img
-          className={styles.dashboard2Icon5}
-          loading="lazy"
-          alt=""
-          src="/dashboard-2-5.svg"
-        />
-        <div className={styles.dashboard5}>Log-out</div>
+        <img className={styles.dashboard2Icon5} alt="" src="/dashboard-2.svg" />
+        <div className={styles.dashboard5}>Dashboard</div>
       </div>
     </div>
   );
